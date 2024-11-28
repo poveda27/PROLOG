165cba743fe97280df6962b7966631aab49f4208
poveda27 <116394608+poveda27@users.noreply.github.com>
2024-11-27 22:38:13 -0500

    Update Laberinto.pl

--- PROLOG_Ejercicios/Ejercicio_Laberinto/Laberinto.pl
+++ PROLOG_Ejercicios/Ejercicio_Laberinto/Laberinto.pl
@@ -9,7 +9,13 @@ conectado(11,15).
 conectado(10,9).
 conectado(9,5).
 conectado(15,19).
-entrada(1).
-salidad(19).
+conectado(5,6).
+conectado(6,7).
+conectado(9,13).
+conectado(13,17).
+conectado(17,18).
+conectado(18,14).
+conectado(19,20).
+conectado(20,16).
 ruta(X,Y):- conectado(X,Y).
 ruta(X,Y):- conectado(X,A),ruta(A,Y).
